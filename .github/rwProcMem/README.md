d726b7e4ff22d74d19b2428f03288c9ceb921f91
MoeLeak <hacksb888@gmail.com>
2025-02-12 21:17:52 +0800

    Ready for rwProcMem

---
+++ .github/rwProcMem/README.md
@@ -0,0 +1,54 @@
+# 驱动1名称：Linux ARM64内核硬件进程内存读写驱动37
+本驱动支持所有能解锁BL的手机，无论小米、黑鲨、红魔、ROG、一加、三星、摩托罗拉等等，并且不需要手机厂商开放内核源码。只需要手动修改五六处地方，就可以跑在任意机型的内核上！具体修改过程不再本文章的论述中。本文章仅提供驱动源代码。
+
+### 本驱动接口列表：
+1. 驱动_设置驱动设备接口文件允许同时被使用的最大值: SetMaxDevFileOpen
+2. 驱动_隐藏驱动（卸载驱动需重启机器）: HideKernelModule
+3. 驱动_打开进程: OpenProcess
+4. 驱动_读取进程内存: ReadProcessMemory
+5. 驱动_写入进程内存: WriteProcessMemory
+6. 驱动_关闭进程: CloseHandle
+7. 驱动_获取进程内存块列表: VirtualQueryExFull（可选：显示全部内存、只显示在物理内存中的内存）
+8. 驱动_获取进程PID列表: GetProcessPidList
+9. 驱动_获取进程权限等级: GetProcessGroup
+10. 驱动_提升进程权限到Root: SetProcessRoot
+11. 驱动_获取进程占用物理内存大小: GetProcessRSS
+12. 驱动_获取进程命令行: GetProcessCmdline
+
+
+# 驱动2名称: Linux ARM64内核硬件断点进程调试驱动3
+### 本驱动接口列表：
+1.  驱动_打开进程: OpenProcess
+2.  驱动_关闭进程: CloseHandle
+3.  驱动_获取CPU支持硬件执行断点的数量: GetNumBRPS
+4.  驱动_获取CPU支持硬件访问断点的数量: GetNumWRPS
+5.  驱动_设置进程硬件断点: AddProcessHwBp
+6.  驱动_删除进程硬件断点: DelProcessHwBp
+7.  驱动_暂停硬件断点: SuspendProcessHwBp
+8.  驱动_恢复硬件断点: ResumeProcessHwBp
+9.  驱动_读取硬件断点命中信息: ReadHwBpInfo
+10. 驱动_设置无条件Hook跳转: SetHookPC
+
+## 目录说明：
+> **rwProcMem33Module**：进程内存读写驱动
+>>* **rwProcMem_module**：（*内核层*）驱动源码
+>>* **testKo**：（*应用层*）调用驱动demo
+>>* **testTarge**t：（*应用层*）读取第三方进程demo
+>>* **testMemSearch**：（*应用层*）搜索第三方进程内存demo
+>>* **testDumpMem**：（*应用层*）保存第三方进程内存demo
+>>* **testCEServer**：（*应用层*）CheatEngine远程服务器，可配合CheatEngine7.1远程连接使用
+
+> **hwBreakpointProcModule**：硬件断点进程调试驱动
+>>* **hwBreakpointProc_module**：（*内核层*）驱动源码
+>>* **testHwBp**：（*应用层*）调用驱动demo
+>>* **testHwBpTarget**：（*应用层*）硬件断点第三方进程demo
+>>* **testHwBpClient**：（*应用层*）硬件断点工具之远程客户端
+>>* **testHwBpServer**：（*应用层*）硬件断点工具之远程服务端
+
+## 更新日志（2024-10）：
+1.支持Linux5.10、6.1
+
+2.修复硬件断点会卡死的bug
+
+3.修复搜索Kit的一些bug
+
---
+++ .github/rwProcMem/proc_root_auto_offset.h
@@ -0,0 +1,101 @@
+﻿#ifndef PROC_ROOT_AUTO_OFFSET_H_
+#define PROC_ROOT_AUTO_OFFSET_H_
+#include <linux/ctype.h>
+#include "ver_control.h"
+
+MY_STATIC ssize_t g_real_cred_offset_proc_root = 0;
+MY_STATIC bool g_init_real_cred_offset_success = false;
+
+MY_STATIC inline int init_proc_root_offset(const char* proc_self_status_content) {
+	size_t len = 0;
+	char* lp_tmp_line = NULL;
+	char* lp_tmp_name = NULL;
+
+	char comm[TASK_COMM_LEN] = { 0 }; //executable name excluding path
+	char* lp_find_line_single = NULL;
+	const ssize_t offset_lookup_min = -100;
+	const ssize_t offset_lookup_max = 300;
+	const ssize_t min_real_cred_offset_safe = offset_lookup_min + sizeof(void*) * 3;
+
+	//有些内核没有导出get_task_comm，所以不能使用
+	//get_task_comm(comm, task);
+
+	lp_find_line_single = strchr(proc_self_status_content, '\n');
+	if (!lp_find_line_single) {
+		return 0;
+	}
+
+	len = ((size_t)lp_find_line_single - (size_t)proc_self_status_content);
+	lp_tmp_line = (char*)__kmalloc(len + 1, GFP_KERNEL);
+	lp_tmp_name = (char*)__kmalloc(len + 1, GFP_KERNEL);
+	if (!lp_tmp_line || !lp_tmp_name) {
+		return -ENOMEM;
+	}
+	memset(lp_tmp_line, 0, len + 1);
+	memset(lp_tmp_name, 0, len + 1);
+	memcpy(lp_tmp_line, proc_self_status_content, len);
+
+	printk_debug(KERN_INFO "lp_tmp_line:%s\n", lp_tmp_line);
+
+	sscanf(lp_tmp_line, "Name: %s", lp_tmp_name);
+	strlcpy(comm, lp_tmp_name, sizeof(comm));
+
+	printk_debug(KERN_EMERG "len:%lu, comm:%s\n", strlen(comm), comm);
+
+
+	//
+	//for (i = 0; i < TASK_COMM_LEN; i++)
+	//{
+	//	printk_debug(KERN_EMERG "comm %x\n", comm[i]);
+	//}
+
+
+	kfree(lp_tmp_line);
+	kfree(lp_tmp_name);
+
+
+	g_init_real_cred_offset_success = false;
+
+	for (g_real_cred_offset_proc_root = offset_lookup_min; g_real_cred_offset_proc_root <= offset_lookup_max; g_real_cred_offset_proc_root++) {
+
+		char* lpszComm = (char*)&current->real_cred;
+		lpszComm += g_real_cred_offset_proc_root;
+
+		printk_debug(KERN_EMERG "curent g_real_cred_offset_proc_root:%zd, bytes:%x\n", g_real_cred_offset_proc_root, *(unsigned char*)lpszComm);
+
+		if(g_real_cred_offset_proc_root < min_real_cred_offset_safe) {
+			continue;
+		}
+		if (strcmp(lpszComm, comm) == 0) {
+			ssize_t maybe_real_cred_offset = g_real_cred_offset_proc_root - sizeof(void*) * 2;
+			char * p_test_mem1 = (char*)&current->real_cred + maybe_real_cred_offset;
+			char * p_test_mem2 = (char*)&current->real_cred + maybe_real_cred_offset + sizeof(void*);
+			if(memcmp(p_test_mem1, p_test_mem2, sizeof(void*)) != 0 ) {
+				maybe_real_cred_offset = g_real_cred_offset_proc_root - sizeof(void*) * 3;
+				p_test_mem1 = (char*)&current->real_cred + maybe_real_cred_offset;
+				p_test_mem2 = (char*)&current->real_cred + maybe_real_cred_offset + sizeof(void*);
+				if(memcmp(p_test_mem1, p_test_mem2, sizeof(void*)) != 0 ) {
+					break; // failed
+				}
+			}
+
+			g_real_cred_offset_proc_root = maybe_real_cred_offset;
+
+			printk_debug(KERN_EMERG "strcmp found %zd\n", g_real_cred_offset_proc_root);
+
+			g_init_real_cred_offset_success = true;
+			break;
+		}
+
+	}
+
+	if (!g_init_real_cred_offset_success) {
+		printk_debug(KERN_INFO "real_cred offset failed\n");
+		return -ESPIPE;
+	}
+	printk_debug(KERN_INFO "g_real_cred_offset_proc_root:%zu\n", g_real_cred_offset_proc_root);
+	return 0;
+}
+#endif /* PROC_ROOT_AUTO_OFFSET_H_ */
+
+
